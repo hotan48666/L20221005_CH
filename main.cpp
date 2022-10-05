f31625efaabcece2947e341efb277f0adcf25bcf
hotan48666 <68625837+hotan48666@users.noreply.github.com>
2022-10-05 15:10:46 +0900

    inCodeing add..

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 #define IS_VALID(A) (A != nullptr)
 #define SAFE_DELETE(A) if(A) { delete A; A = nullptr; }
+//if(A) 이게 값이 있는지 확인 1 
+// A에 값이 있으면 true 이고 없으면 false 
+// delete A 삭제 2
+// A = nullptr 삭제해도 쓰레기값 있을수 있어 초기화 3
 
 #define _SAFE_DELETE(Player)	delete Player; \
 								Player = nullptr; \
